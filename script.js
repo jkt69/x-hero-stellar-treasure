058a7256386c4314563de7cecac7e630be8891e5
jkt69 <linken.sphere@gmail.com>
2024-05-20 17:18:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-class Box {
-    constructor(biru, ungu, kuning, merah) {
-        this.biru = biru;
-        this.ungu = ungu;
-        this.kuning = kuning;
-        this.merah = merah;
-    }
+function calculatePoints() {
+    const biru = parseInt(document.getElementById('biru').value);
+    const ungu = parseInt(document.getElementById('ungu').value);
+    const kuning = parseInt(document.getElementById('kuning').value);
+    const merah = parseInt(document.getElementById('merah').value);
+
+    const totalPoints = (biru * 2) + (ungu * 10) + (kuning * 20) + (merah * 50);
+    const totalBoxes = biru + ungu + kuning + merah;
+
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = `
+        <p>Total Points: ${totalPoints}</p>
+        <p>Total Boxes Opened: ${totalBoxes}</p>
+    `;
 }
